484c3d885ed27edab46e18fabeb6a3cb99a2ad02
milanakuchumova <62593415+milanakuchumova@users.noreply.github.com>
2024-01-05 18:26:48 +0300

    try commit

--- train.py
+++ train.py
@@ -1,17 +1,5 @@
-import pandas as pd
-import fire
 import dvc.api
 
-def read_data(filename: str):
-    df = pd.read_csv(filename)
-    
-
-def main():
-    url = dvc.api.get_url('train_data.csv',
-        repo='gdrive://1glMslxpGLmz_GwYXUdiy5hr5C2vEIW_Q/files')
-    print(url)
-
-
-if __name__ == '__main__':
-    main()
-    # fire.Fire(main)
+df = dvc.api.read('train_data.csv',
+                  repo='gdrive://1glMslxpGLmz_GwYXUdiy5hr5C2vEIW_Q')
+print(df)
