70dd2ec6ff19c892f466f3f70e3ec49d1ff65408
milanakuchumova <62593415+milanakuchumova@users.noreply.github.com>
2024-01-07 19:40:19 +0300

    try clone dvc

--- train.py
+++ train.py
@@ -1,8 +1,9 @@
-import dvc.api
 import pandas as pd
 
-with dvc.api.open('data/train_data.csv',
-                  repo='https://github.com/milanakuchumova/mlops_hw.git') as f:
-    
-    df = pd.read_csv(f)
-    print(df.head())
+from dvc.repo import Repo
+
+repo = Repo(".")
+repo.pull()
+
+
+
